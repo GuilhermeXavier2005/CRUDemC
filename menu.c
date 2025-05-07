6be62c9e2b9f239295c327a01c7e249131aba379
Enrico Nascimento SIlva <enriconascimento9@gmail.com>
2025-05-07 20:08:15 -0300

    Cria o menu principal da aplicacao
    Implementa a função menu que exibe as opções disponíveis para o usuário no sistema de controle de estoque.

---
+++ menu.c
@@ -0,0 +1,16 @@
+#include "header/header.h"
+
+void menu() {
+    system("clear");
+    printf("\n------| Sistema de Controle de Estoque |------\n");
+    printf("1. Cadastrar Novo Produto\n");
+    printf("2. Registrar Compras\n");
+    printf("3. Registrar Vendas)\n");
+    printf("4. Listar Todos os Produtos\n");
+    printf("5. Consultar Produto (por Codigo ou Nome)\n");
+    printf("6. Alterar Produto\n");
+    printf("7. Deletar Produto\n");
+    printf("8. Mostrar relatorio de estoque\n");
+    printf("0. Sair e Salvar\n");
+    printf("------------------------------------------------\n");
+  }
